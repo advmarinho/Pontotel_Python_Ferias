50883f67180c5b43ce77fa0aab05320860085ab1
advmarinho <marinho.1983@gmail.com>
2021-12-30 13:23:43 -0300

    Update appSiteE.py

--- appSiteE.py
+++ appSiteE.py
@@ -6,15 +6,16 @@ from openpyxl import load_workbook
 import pandas as pd
 
 # pyinstaller --onefile --noconsole .\nome.py
-# pyinstaller --onefile --console .\appSiteE.py
+# pyinstaller --onefile --console .\appSiteE1.py
 # cxfreeze appSite.py --target-dir dist
 # python -m venv folhasSrcSraSrv criando ambiente virtual
-#deactivate
+# deactivate
 # \Scripts\Activate.ps1 ativando ambiente
 #Atualizando python -m pip install --upgrade pip  e verificar pip list
 
 
 print('Siga as instruções  ')
+print('Software by ADS - Anderson Marinho  \n\n')
 abrirFerias0 = input(r'Digite Caminho da SRR férias: ') + str('\\SRR.xlsx')
 abrirFerias0 = pd.read_excel(abrirFerias0, sheet_name='SRR', index_col=None, header=None)
 print(abrirFerias0.head())
@@ -26,21 +27,25 @@ nome_arquivo = abrirFerias
 print(nome_arquivo)
 print("\n\n\t |   --> Robô ENCONTROU esses dados para incluir Férias <--                   \n")
 
-
+# int(input("Quantas férias devem ser lançadas: "))
 planilha_aberta = load_workbook(filename=nome_arquivo)
 sheet_selecionada = planilha_aberta['SRR']
 linhaWS = input("A partir da linha tal: ")
-qtdlinhasSite = int(input("Quantas férias devem ser lançadas: "))
+qtdlenWS = len(abrirFerias0)
+qtdlinhasSite = int(qtdlenWS) - int(linhaWS) + 1
+print('Serão enviadas essa(s) quantidade de linhas ', qtdlinhasSite)
+print('\nRecomenda-se 30 segundos para cada linha')
+segundosUsar = int(input("Quantos segundos devem ser usados no preenchimento das férias no site: "))
 
 login = input("Digite o login: ")
 senha = getpass.getpass("Digite a senha: ")
 
-chrome_driver = webdriver.Chrome(input(r'Digite Caminho da CHROME férias: ') + str('\\chromedriver.exe'))
+chrome_driver = webdriver.Chrome(input(r'Digite Caminho do CHROME férias: ') + str('\\chromedriver.exe'))
 
 newSite = "https://gestao.pontotel.com.br/#/cognito/login"
 chrome_driver.get(newSite)
 chrome_driver.maximize_window()
-sleep(5)
+sleep(4)
 texto = chrome_driver.find_element_by_xpath('//*[@id="email"]')
 sleep(0.5)
 abrirsite.write(str(login))
@@ -48,9 +53,9 @@ sleep(0.5)
 abrirsite.press('enter')
 sleep(0.5)
 texto = chrome_driver.find_element_by_xpath('//*[@id="view-space"]/div/div/div[1]/form/div[2]/input')
-sleep(1)
+sleep(0.5)
 abrirsite.write(senha)
-sleep(1)
+sleep(0.5)
 abrirsite.press('enter')
 sleep(4)
 abrirsite.press('esc', 3)
@@ -74,16 +79,37 @@ while(contador1 < int(qtdlinhasSite) + 1):
         
         
         chrome_driver.get('https://gestao.pontotel.com.br/#/employee/list')
-        sleep(3)
+        sleep(2)
         chrome_driver.find_element_by_xpath('//*[@id="view-space"]/div/div[2]/div[1]/div[1]/div[3]/div[1]/input').send_keys(matricula)
-        sleep(3)
+        sleep(2)
         chrome_driver.find_element_by_xpath('//*[@id="employee-list-row-0"]/div[1]/span/span').click()
-        sleep(1.5)
+        sleep(2)
         chrome_driver.find_element_by_xpath('//*[@id="view-space"]/div/div[2]/div[1]/div[3]/div[2]/div/div[1]/div/div[10]/div[5]').click()
-        sleep(1.5)
+        sleep(2)
         contador1 = contador1 + 1
         print(contador1)
-        input('Pressione enter para continuar ')
+        def cronometro(i, f, p):
+            print(f'Cronometro de {i} até {f} de {p} em {p}')
+            sleep(2)
+            if i < f:
+                cont = i
+                while cont <= f:
+                    print(f'{cont} ', end='-', flush=True)
+                    sleep(0.5)
+                    cont += p
+                print(' Outro funcionário')
+            else:
+                cont = i
+                while cont >= f:
+                    print(f'{cont} ', end='', flush=True)
+                    sleep(0.5)
+                    cont -= p
+                print(' Outro funcionário')
+        cronometro((segundosUsar), 0, 1)
+        
+        abrirsite.press('enter')
+        print('Seguindo para novo funcionário ')
+        sleep(2)
 
         
 else:
